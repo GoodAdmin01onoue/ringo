4a3332629af6245c46d9c3410e224c446abf652f
user01 <example@example.com>
2018-10-26 12:15:25 +0900

    ログインのサーブレットとJSPを追加

---
+++ src/controllers/LoginServlet.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		//logion.jspに移動
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+		rd.forward(request, response);
+
+	}
+}
