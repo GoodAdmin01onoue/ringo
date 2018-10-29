6febba7c253753449ccac6fccfb2e3bae4c666f1
user01 <example@example.com>
2018-10-29 11:11:59 +0900

    メッセージ表示のスタイルシート修正

--- src/controllers/LoginServlet.java
+++ src/controllers/LoginServlet.java
@@ -18,6 +18,10 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		if(request.getAttribute("error") != null) {
+
+			request.removeAttribute("error");
+		}
 
 		//logion.jspに移動
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
@@ -57,21 +61,24 @@ public class LoginServlet extends HttpServlet {
 			login_user = ld.logincheck(name, pass);
 
 
-			if(login_user == null) {
+			if((login_user.getUserName() == null || !login_user.getUserName().equals(name))
+					&& (login_user.getLoginPw() == null || !login_user.getLoginPw().equals(pass))) {
 				//検索結果がnullの場合
 				//エラーメッセージを設定
 				String error2 = "名前をパスワードが一致しません";
 				//エラーメッセージをリクエストオブジェクトに代入
 				request.setAttribute("error", error2);
+				//login.jspに戻る
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+				rd.forward(request, response);
 
 			} else {
 				//セッション開始
 				HttpSession session = request.getSession(true);
 				//セッションオブジェクトにログインユーザー情報を格納
 				session.setAttribute("loginUser", login_user);
-				//検索画面へ移動
-				RequestDispatcher rd = request.getRequestDispatcher("/search");
-				rd.forward(request, response);
+				//検索画面へリダイレクト
+				response.sendRedirect(request.getContextPath() + "/search");
 			}
 		}
 	}
